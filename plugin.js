cb5f4c0c74b88a9672fdc61e74fe63dfd5b19c66
marcopiraccini <marco.piraccini@gmail.com>
2022-12-13 16:08:21 +0100

    test gh action

--- plugin.js
+++ plugin.js
@@ -4,6 +4,6 @@ module.exports = async function plugin (app) {
   app.log.info('plugin loaded')
 
   app.get('/hello', async function (request, response) {
-    return { hello: 'from TEST- PR -1' }
+    return { hello: 'from TEST- PR -1 - NEW' }
   })
 }
