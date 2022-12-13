6e7e3a4a189c4e559cd8b1b2a1e3aa6328f195de
marcopiraccini <marco.piraccini@gmail.com>
2022-12-13 09:49:08 +0100

    pr1 -test

--- plugin.js
+++ plugin.js
@@ -4,6 +4,6 @@ module.exports = async function plugin (app) {
   app.log.info('plugin loaded')
 
   app.get('/hello', async function (request, response) {
-    return { hello: 'from PR 5 -v5' }
+    return { hello: 'from TEST- PR -1' }
   })
 }
